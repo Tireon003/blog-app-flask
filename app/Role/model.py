9a225cfd8d4c7dc328154a0fee44c4156ace6123
Tireon003 <gunash29@mail.ru>
2024-10-02 01:49:39 +0300

    create role model

---
+++ app/Role/model.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.User import User
+
+from app.core import Base
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+    users: Mapped[list["User"]] = relationship(
+        back_populates="role"
+    )
