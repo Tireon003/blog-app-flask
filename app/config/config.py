8a7a9e630c7c368c6afb847681eb09fabaf8c576
Tireon003 <gunash29@mail.ru>
2024-10-02 00:39:00 +0300

    config settings

---
+++ app/config/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    SECRET_KEY: str
+
+    @property
+    def db_url(self) -> str:
+        return (f'postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}'
+                f'@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}')
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
