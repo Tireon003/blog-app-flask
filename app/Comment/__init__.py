edde3b34d9fb5982947a76c3817b6a723372d4be
Tireon003 <gunash29@mail.ru>
2024-10-07 00:27:46 +0300

    add service to init

--- app/Comment/__init__.py
+++ app/Comment/__init__.py
@@ -1,7 +1,9 @@
 from .model import Comment
 from .repository import CommentRepository
+from .services import CommentService
 
 __all__ = (
     'Comment',
     'CommentRepository',
+    'CommentService',
 )
