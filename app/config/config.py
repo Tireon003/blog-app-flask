a1927e7d966ded1656cf3585d2efbdabd439663a
Tireon003 <gunash29@mail.ru>
2024-10-09 01:41:57 +0300

    modified url property for migrations

--- app/config/config.py
+++ app/config/config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     @property
     def db_url_migrations(self) -> str:
         return (f'postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}'
-                f'@{self.POSTGRES_HOST}:5432/{self.POSTGRES_DB}')
+                f'@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}')  # port 5678 for connection outside docker
 
     model_config = SettingsConfigDict(env_file='.env')
 
